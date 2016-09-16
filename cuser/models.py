db0ea5f9e8ff0827f562497dc447be05e253a261
Adam Taylor <ataylor32@gmail.com>
2016-09-15 20:22:53 -0600

    Add Django's usual translations to the "AbstractCUser" class

--- cuser/models.py
+++ cuser/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import (
 )
 from django.core.mail import send_mail
 from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 
 class CUserManager(BaseUserManager):
@@ -45,25 +46,30 @@ class AbstractCUser(AbstractBaseUser, PermissionsMixin):
     Email and password are required. Other fields are optional.
     """
     email = models.EmailField(
-        'email',
+        _('email address'),
         unique=True,
         error_messages={
             'unique': "A user with that email address already exists.",
         },
     )
-    first_name = models.CharField('first name', max_length=30, blank=True)
-    last_name = models.CharField('last name', max_length=30, blank=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
     is_staff = models.BooleanField(
-        'staff status',
+        _('staff status'),
         default=False,
-        help_text='Designates whether the user can log into this admin site.',
+        help_text=_(
+            'Designates whether the user can log into this admin site.'
+        ),
     )
     is_active = models.BooleanField(
-        'active',
+        _('active'),
         default=True,
-        help_text='Designates whether this user should be treated as active. Unselect instead of deleting accounts.',
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
     )
-    date_joined = models.DateTimeField('date joined', default=timezone.now)
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
     objects = CUserManager()
 
@@ -71,8 +77,8 @@ class AbstractCUser(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name = 'user'
-        verbose_name_plural = 'users'
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
         abstract = True
 
     def __str__(self):
