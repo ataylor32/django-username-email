30ebd180c6cbea0613e5354b59642d152594ad66
Tom Meagher <thom.meagher@gmail.com>
2016-11-19 11:50:50 -0500

    Publish pages site

--- cuser/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
-
-
-class CuserConfig(AppConfig):
-    name = 'cuser'
-    verbose_name = _("Custom User")
