cf6efe32468dd507585bb16028c8dab548ef57a8
Adam Taylor <ataylor32@gmail.com>
2017-09-04 15:19:40 -0600

    Bumped version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst')) as f:
 setup(
     name='django-username-email',
 
-    version='2.1.0',
+    version='2.1.1',
 
     description='Custom Django User model that makes email the USERNAME_FIELD.',
     long_description=long_description,
