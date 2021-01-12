afe924cd5051e1da36e030759214470e6ab733a4
Adam Taylor <ataylor32@gmail.com>
2021-01-11 19:08:51 -0700

    Replaced all instances of ugettext_lazy with gettext_lazy

--- cuser/admin.py
+++ cuser/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group as StockGroup
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from cuser.forms import UserChangeForm, UserCreationForm
 from cuser.models import CUser, Group
--- cuser/forms.py
+++ cuser/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.contrib.auth import (authenticate, get_user_model,
                                  password_validation)
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from cuser.models import CUser
 
