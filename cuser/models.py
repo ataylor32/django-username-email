1ec719debba8b928a679213e7afe15636c8a8779
Adam Taylor <ataylor32@gmail.com>
2021-01-11 19:08:51 -0700

    Replaced all instances of ugettext_lazy with gettext_lazy

--- cuser/models.py
+++ cuser/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import (
 from django.contrib.auth.models import Group as BaseGroup
 from django.core.mail import send_mail
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class CUserManager(BaseUserManager):
