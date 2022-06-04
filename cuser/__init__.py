69ad65c4d1b0602ba4f2909c233c8b47169a09ad
Adam Taylor <ataylor32@gmail.com>
2022-06-03 20:34:36 -0600

    Changes for Django 3.2

--- cuser/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'cuser.apps.CuserConfig'
--- cuser/apps.py
+++ cuser/apps.py
@@ -5,3 +5,4 @@ from .settings import CUSER_SETTINGS
 class CuserConfig(AppConfig):
     name = 'cuser'
     verbose_name = CUSER_SETTINGS['app_verbose_name']
+    default_auto_field = 'django.db.models.AutoField'
