6935c93546ab7116925207d6e442323c1656d46e
Adam Taylor <ataylor32@gmail.com>
2024-09-18 19:06:59 -0600

    Bumped version to 2.5.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst')) as f:
 setup(
     name='django-username-email',
 
-    version='2.5.5',
+    version='2.5.6',
 
     description='Custom Django User model that makes email the USERNAME_FIELD.',
     long_description=long_description,
